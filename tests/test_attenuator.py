bc3547da5ddf097a5e339deb0f9e5a6273bf1593
Dan Henriksen <dhenrikse@bnl.gov>
2026-05-08 22:25:30 -0400

    added test, added test env dependency

--- tests/test_attenuator.py
+++ tests/test_attenuator.py
@@ -1,17 +1,19 @@
 from __future__ import annotations
 
 import pytest_asyncio
-from ophyd_async.core import init_devices
+from ophyd_async.core import init_devices, callback_on_mock_put, set_mock_value
 
-from cditools.attenuator import AttenuatorBank
+from cditools.attenuator import AttenuatorBank, AVAILABLE_ATTENUATIONS
 
 pytest_plugins = ("pytest_asyncio",)
 
 
 @pytest_asyncio.fixture
 async def mock_attenuator_bank():
+
     async with init_devices(mock=True):
         mock_attenuator_bank = AttenuatorBank()
+
     yield mock_attenuator_bank
 
 
@@ -27,3 +29,6 @@ def test_find_closest_attenuation(mock_attenuator_bank: AttenuatorBank):
 
     nearest4 = mock_attenuator_bank.find_closest_attenuation(0.98)
     assert nearest4.attenuation == 1
+
+def test_up_to_date_available_attenuations(mock_attenuator_bank: AttenuatorBank):
+    assert mock_attenuator_bank._calculate_available_attentuations() == AVAILABLE_ATTENUATIONS # type: ignore

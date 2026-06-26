a3e6656dae678729948bc125b3adc17878de39de
Thomas A Caswell <tcaswell@bnl.gov>
2026-06-25 20:36:05 -0400

    FIX: and the other trailing whitespace

--- src/cditools/merlin_async.py
+++ src/cditools/merlin_async.py
@@ -44,7 +44,7 @@ class MerlinTriggerMode(StrictEnum):
     TRIGGER_ENABLE = "Trigger Enable"
     TRIGGER_START_RISING = "Trigger start rising"
     TRIGGER_START_FALLING = "Trigger start falling"
-    TRIGGER_BOTH_RISING = "Trigger both rising "
+    TRIGGER_BOTH_RISING = "Trigger both rising"
     SOFTWARE = "Software"
 
 

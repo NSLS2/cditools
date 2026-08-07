fd29f3a22c934fa216e42b4f9d13039fba3eeed3
Dan Henriksen <dhenrikse@bnl.gov>
2026-08-07 14:37:45 -0400

    fixed tests

--- tests/test_merlin_async.py
+++ tests/test_merlin_async.py
@@ -1,20 +1,16 @@
-from dataclasses import dataclass
-
-from bluesky.run_engine import RunEngine
-from ophyd_async.core import init_devices
 import pytest
-
-from cditools.eiger_async import EigerDriverIO
+from dataclasses import dataclass
+from bluesky.run_engine import RunEngine
+from ophyd_async.core import init_devices, set_mock_value
 from ophyd_async.epics.adcore import ADBaseDataType
-
-from cditools.merlin_async import MerlinCounterDepth
+from cditools.merlin_async import MerlinCounterDepth, MerlinDriverIO
 
 
 @pytest.fixture
-def mock_merlin_driver(RE: RunEngine) -> EigerDriverIO:
+def mock_merlin_driver(RE: RunEngine) -> MerlinDriverIO:
     """Create a mock EigerDriverIO for testing."""
     with init_devices(mock=True):
-        driver = EigerDriverIO("MOCK:EIGER:cam1:")
+        driver = MerlinDriverIO("MOCK:EIGER:cam1:")
 
     @dataclass
     class Parent:
@@ -23,12 +19,12 @@ def mock_merlin_driver(RE: RunEngine) -> EigerDriverIO:
     driver.parent = Parent("merlin")
 
     # Set some mock values
-    driver.counter_depth = MerlinCounterDepth.BIT_12
+    set_mock_value(driver.counter_depth, MerlinCounterDepth.BIT_12)
 
     return driver
 
-def test_driver_data_type(mock_merlin_driver: EigerDriverIO):
-    assert mock_merlin_driver.data_type == ADBaseDataType.UINT16
+async def test_driver_data_type(mock_merlin_driver: MerlinDriverIO):
+    assert await mock_merlin_driver.data_type.get_value() == ADBaseDataType.UINT16
 
-    mock_merlin_driver.counter_depth = MerlinCounterDepth.BIT_24
-    assert mock_merlin_driver.data_type == ADBaseDataType.UINT32
+    set_mock_value(mock_merlin_driver.counter_depth, MerlinCounterDepth.BIT_24)
+    assert await mock_merlin_driver.data_type.get_value() == ADBaseDataType.UINT32

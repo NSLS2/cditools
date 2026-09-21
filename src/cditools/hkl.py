94c05874f25bbb7b377eee64431c17a0020103e1
Joseph Hanrahan <jhanrahan@bnl.gov>
2026-09-21 15:25:23 -0400

    try new approach

--- src/cditools/hkl.py
+++ src/cditools/hkl.py
@@ -34,33 +34,33 @@ class PseudoMonochromator(WavelengthXray):
     def _energy_changed(self, value: float, **_kwargs: Any) -> None:
         self.energy.put(value)
 
-class CDIDiffractometer(DiffractometerBase):
+# class CDIDiffractometer(DiffractometerBase):
 
-    def __init__(self, gon: GON, energy: Energy, **kwargs):
-        # configure goniometer ophyd signals
-        self.mu = gon.sam.ry
-        self.chi = gon.sam.c_lg.lrx
-        self.phi = gon.sam.c_lg.lrz
-        self.omega2 = gon.sam.c_sm.lrx
-        self.chi2 = gon.sam.c_sm.lrz
+#     def __init__(self, gon: GON, energy: Energy, **kwargs):
+#         # configure goniometer ophyd signals
+#         self.mu = gon.sam.ry
+#         self.chi = gon.sam.c_lg.lrx
+#         self.phi = gon.sam.c_lg.lrz
+#         self.omega2 = gon.sam.c_sm.lrx
+#         self.chi2 = gon.sam.c_sm.lrz
 
-        # configure detector ophyd signals
-        # self.gamma1 = ...
-        # self.delta1 = ...
+#         # configure detector ophyd signals
+#         # self.gamma1 = ...
+#         # self.delta1 = ...
 
-        # configure energy readout from Energy device
-        # self.beam = {'class': PseudoMonochromator,
-        #              'energy_device': energy}
+#         # configure energy readout from Energy device
+#         # self.beam = {'class': PseudoMonochromator,
+#         #              'energy_device': energy}
 
-        super().__init__(
-            prefix='',
-            name='cdi',
-            geometry=register_geometry_file(CUSTOM_GEOMETRY_YAML_PATH),
-            solver='ad_hoc',
-            reals=["mu", "chi", "phi", "omega2", "chi2", "gamma1", "delta1"],
-            pseudos=["h", "k", "l"],
-            **kwargs
-        )
+#         super().__init__(
+#             prefix='',
+#             name='cdi',
+#             geometry=register_geometry_file(CUSTOM_GEOMETRY_YAML_PATH),
+#             solver='ad_hoc',
+#             reals=["mu", "chi", "phi", "omega2", "chi2", "gamma1", "delta1"],
+#             pseudos=["h", "k", "l"],
+#             **kwargs
+#         )
 
 # def initialize_hkl_diffractometer(name: str = "cdi") -> DiffractometerBase:
 #     # initialize hklpy2 diffractometer
@@ -83,5 +83,65 @@ class CDIDiffractometer(DiffractometerBase):
     
     
 #     return cdi
+
+from typing import ClassVar
+
+from ophyd import Component as Cpt
+from ophyd import EpicsMotor
+from ophyd import Kind
+from ophyd import SoftPositioner
+
+import hklpy2
+from hklpy2.diffract import Hklpy2PseudoAxis
+from hklpy2.incident import EpicsMonochromatorRO
+
+NORMAL_HINTED = Kind.hinted | Kind.normal
+
+
+class CDIDiffractometer(DiffractometerBase):
+
+    beam = Cpt(
+        EpicsMonochromatorRO,
+        "",
+        source_type="Simulated read-only EPICS Monochromator",
+        pv_energy="BraggERdbkAO",  # the energy readback PV
+        energy_units="keV",
+        pv_wavelength="BraggLambdaRdbkAO",  # the wavelength readback PV
+        wavelength_units="angstrom",
+        wavelength_deadband=0.000_150,
+        kind=NORMAL_HINTED,
+    )
+
+    # Pseudo-space axes, in order expected by hkl_soleil E4CV, engine="hkl"
+    h = Cpt(Hklpy2PseudoAxis, "", kind=NORMAL_HINTED)
+    k = Cpt(Hklpy2PseudoAxis, "", kind=NORMAL_HINTED)
+    l = Cpt(Hklpy2PseudoAxis, "", kind=NORMAL_HINTED)
+
+    # Real-space axes, in our own order..
+    # Use different names than the solver for some axes
+    mu = Cpt(EpicsMotor, "Gon:1-Ax:Ry}Mtr", kind=NORMAL_HINTED)
+    chi = Cpt(EpicsMotor, "Gon:1-Ax:Rx2}Mtr", kind=NORMAL_HINTED)
+    phi = Cpt(EpicsMotor, "Gon:1-Ax:Rz2}Mtr", kind=NORMAL_HINTED)
+    omega2 = Cpt(EpicsMotor, "Gon:1-Ax:Rx1}Mtr", kind=NORMAL_HINTED)
+    chi2 = Cpt(EpicsMotor, "Gon:1-Ax:Rz1}Mtr", kind=NORMAL_HINTED)
+    gamma1 = Cpt(EpicsMotor, "", kind=NORMAL_HINTED)
+    delta1 = Cpt(EpicsMotor, "", kind=NORMAL_HINTED)
+
+    # Just the axes in expected order by hkl_soleil E4CV.
+    _pseudo: ClassVar[list[str]] = ["h", "k", "l"]
+    _real: ClassVar[list[str]] = ["mu", "chi", "phi", "omega2", "chi2", "gamma1", "delta1"]
+
+
+    def __init__(self, **kwargs):
+        # kwargs["prefix"] = prefix
+        super().__init__(
+            prefix="XF:09IDC-OP:1{",
+            solver="ad_hoc",
+            name='cdi',
+            geometry=register_geometry_file(CUSTOM_GEOMETRY_YAML_PATH),
+            pseudos=["h", "k", "l"],
+            reals=["mu", "chi", "phi", "omega2", "chi2", "gamma1", "delta1"],
+            **kwargs,
+        )
 
 
